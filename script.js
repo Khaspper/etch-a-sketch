13305512441622094b949eac332c3a96afdd5b63
Markus <brandonnarciso24@gmail.com>
2023-07-27 20:14:28 -0700

    Add divs directly
    instead of using an array just add them directly this is a lot
    less complicated

--- script.js
+++ script.js
@@ -1,68 +1,13 @@
 let canvas = document.getElementById("canvas");
-const content = document.getElementById("content");
-const slider = document.getElementById("slider");
-let pixels = 16;
-let dimension = document.getElementById("dimension");
-slider.addEventListener("mouseup", (event) => {
-   pixels = slider.value;
-   addPixels(canvas);
-})
+let pixelCount = 16;
 
-const row = [];
-
-let isMouseDown = false;
-
-function handleMouseDown(event) {
-   isMouseDown = true;
+for (let i = 0; i < (pixelCount * pixelCount); i++) {
+   let pixel = document.createElement('div');
+   pixel.textContent = i;
+   pixel.style.width = `${512 / pixelCount}px`;
+   pixel.style.height = `${512 / pixelCount}px`;
+   pixel.setAttribute('id', 'box');
+   pixel.style.display = "flex";
+   pixel.style.flexWrap = "wrap";
+   canvas.appendChild(pixel);
 }
-
-function handleMouseUp(event) {
-   isMouseDown = false;
-}
-
-function handleMouseMove(event) {
-   if (isMouseDown) {
-      let targetDiv = document.elementFromPoint(event.clientX, event.clientY);
-      if (targetDiv && targetDiv.classList.contains('box')) {
-         targetDiv.style.backgroundColor = 'black';
-      }
-   }
-}
-
-function addPixels() {
-   deletePixels(canvas);
-   canvas = document.createElement('div');
-   canvas.setAttribute('id', 'canvas');
-   content.appendChild(canvas);
-   
-   for(let i = 0; i < pixels; i++) {
-      row[i] = document.createElement('div');
-      row[i].setAttribute('id', `row_${i}`);
-      row[i].style.display = "flex";
-      row[i].style.width = "100%";
-      for(let j = 0; j < pixels; j++) {
-         row[i][j] = document.createElement('div');
-         row[i][j].setAttribute('id', "box");
-         row[i][j].style.border = "solid 1px";
-         row[i][j].style.flex = "1";
-         row[i].appendChild(row[i][j]);
-      }
-      canvas.appendChild(row[i]);
-   }
-}
-
-function deletePixels(canvas) {
-   for (let i = 0; i < row.length; i++){
-      row.splice(0, row.length);
-   }
-   canvas.remove();
-}
-
-//TODO: Check if you are on the correct file LOL and branch also Create a slider that lets the user choose
-//TODO: how much pixels they want to use and also remember the max is 64px
-
-addPixels(canvas);
-canvas = document.getElementById("canvas");
-canvas.addEventListener("mousedown", handleMouseDown);
-canvas.addEventListener("mousemove", handleMouseMove);
-document.addEventListener("mouseup", handleMouseUp);
