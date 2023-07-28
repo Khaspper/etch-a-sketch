3d5171255c3b98fa7458c27d0cd57ca2718567f8
Markus <brandonnarciso24@gmail.com>
2023-07-27 22:12:45 -0700

    Let user see what pixel they'll choose
    when the user uses the slider it tells them how much pixels they chose

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let canvas = document.getElementById("canvas");
+let pixelDimension = document.getElementById("pixelDimension");
 let isMouseDown = false;
 let pixelCount = 16;
+pixelDimension.textContent = `${pixelCount} X ${pixelCount}`
+slider.addEventListener("mousemove", (event) => pixelDimension.textContent = `${slider.value} X ${slider.value}`)
+slider.addEventListener("mouseup", (event) => {
+   pixelCount = slider.value;
+   deleteAllChildElements(canvas);
+   addPixels(canvas);
+   pixelDimension.textContent = `${pixelCount} X ${pixelCount}`
+})
 
 function handleMouseDown() {
    isMouseDown = true;
@@ -25,17 +34,19 @@ function deleteAllChildElements(parentElement) {
    }
 }
 
-for (let i = 0; i < (pixelCount * pixelCount); i++) {
-   let pixel = document.createElement('div');
-   pixel.textContent = i;
-   pixel.style.width = `${512 / pixelCount}px`;
-   pixel.style.height = `${512 / pixelCount}px`;
-   pixel.classList.add('box');
-   pixel.style.display = "flex";
-   pixel.style.flexWrap = "wrap";
-   canvas.appendChild(pixel);
+function addPixels(canvas) {
+   for (let i = 0; i < (pixelCount * pixelCount); i++) {
+      let pixel = document.createElement('div');
+      pixel.style.width = `${512 / pixelCount}px`;
+      pixel.style.height = `${512 / pixelCount}px`;
+      pixel.classList.add('box');
+      pixel.style.display = "flex";
+      pixel.style.flexWrap = "wrap";
+      canvas.appendChild(pixel);
+   }
 }
 
+addPixels(canvas);
 canvas.addEventListener("mousedown", handleMouseDown);
 canvas.addEventListener("mousemove", handleMouseMove);
 document.addEventListener("mouseup", handleMouseUp);
